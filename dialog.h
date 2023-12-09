10656ee177042cc28be137359017316740cf9311
HonestDeng <2958906959@qq.com>
2023-12-09 13:42:11 +0800

    完成了getToken函数的单元测试

--- dialog.h
+++ dialog.h
@@ -2,20 +2,19 @@
 #define DIALOG_H
 #include <map>
 #include <string>
+
+typedef enum {NUMBER, ID, LPARAM, RPARAM, EQ, MUL, PLUS, MINUS, POWER, LIMIT, DEFINE, SET, CLEAR, SHOW, ERROR, SEMI, COMMA} TokenType;
 class Dialog
 {
-private:
+public:
     double xmin = 0, xmax = 1;
     double step = 0.01;
     std::map<int, double> exp;
-    typedef enum {NUMBER, ID, LPARAM, RPARAM, EQ, MUL, PLUS, MINUS, POWER, LIMIT, DEFINE, SET, CLEAR, SHOW, ERROR, SEMI, COMMA} TokenType;
-
-
     void getToken();
     TokenType token;
     std::string tokenString;
     std::string command;  // 输入的命令
-    int ichar;  // 当前正在读取命令的第ichar个字符
+    int ichar = 0;  // 当前正在读取命令的第ichar个字符
     double cal(double x);
     void setLimit(double l, double r);
     bool parse(std::string& comm);
--- main.cpp
+++ main.cpp
@@ -1,8 +1,91 @@
 #include <iostream>
 #include <string>
+#include <functional>
 #include "dialog.h"
+#include <vector>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(GETTOKEN, DEFINE1) {
+    Dialog dialog;
+    std::string command = "define Y = X;";
+    vector<std::string> expect1 = {"define", "Y", "=", "X", ";"};
+    vector<TokenType> expect2 = {DEFINE, ID, EQ, ID, SEMI};
+    dialog.command = command;
+    for(int i = 0; i < expect1.size(); i++){
+        dialog.getToken();
+        EXPECT_EQ(dialog.tokenString, expect1[i]);
+        EXPECT_EQ(dialog.token, expect2[i]);
+    }
+}
+
+TEST(GETTOKEN, DEFINE2) {
+    Dialog dialog;
+    std::string command = "define Y = 2 * X ^ 3;";
+    vector<std::string> expect1 = {"define", "Y", "=", "2", "*", "X", "^", "3", ";"};
+    vector<TokenType> expect2 = {DEFINE, ID, EQ, NUMBER, MUL, ID, POWER, NUMBER, SEMI};
+    dialog.command = command;
+    for(int i = 0; i < expect1.size(); i++){
+        dialog.getToken();
+        EXPECT_EQ(dialog.tokenString, expect1[i]);
+        EXPECT_EQ(dialog.token, expect2[i]);
+    }
+}
+
+TEST(GETTOKEN, DEFINE3) {
+    Dialog dialog;
+    std::string command = "define Y = 2 * X;";
+    vector<std::string> expect1 = {"define", "Y", "=", "2", "*", "X", ";"};
+    vector<TokenType> expect2 = {DEFINE, ID, EQ, NUMBER, MUL, ID, SEMI};
+    dialog.command = command;
+    for(int i = 0; i < expect1.size(); i++){
+        dialog.getToken();
+        EXPECT_EQ(dialog.tokenString, expect1[i]);
+        EXPECT_EQ(dialog.token, expect2[i]);
+    }
+}
+
+TEST(GETTOKEN, DEFINE4) {
+    Dialog dialog;
+    std::string command = "define Y = X ^ 3;";
+    vector<std::string> expect1 = {"define", "Y", "=", "X", "^", "3", ";"};
+    vector<TokenType> expect2 = {DEFINE, ID, EQ, ID, POWER, NUMBER, SEMI};
+    dialog.command = command;
+    for(int i = 0; i < expect1.size(); i++){
+        dialog.getToken();
+        EXPECT_EQ(dialog.tokenString, expect1[i]);
+        EXPECT_EQ(dialog.token, expect2[i]);
+    }
+}
+
+TEST(GETTOKEN, DEFINE5) {
+    Dialog dialog;
+    std::string command = "define Y = 2;";
+    vector<std::string> expect1 = {"define", "Y", "=", "2", ";"};
+    vector<TokenType> expect2 = {DEFINE, ID, EQ, NUMBER, SEMI};
+    dialog.command = command;
+    for(int i = 0; i < expect1.size(); i++){
+        dialog.getToken();
+        EXPECT_EQ(dialog.tokenString, expect1[i]);
+        EXPECT_EQ(dialog.token, expect2[i]);
+    }
+}
+
+TEST(GETTOKEN, DEFINE6) {
+    Dialog dialog;
+    std::string command = "define Y = 23*X^2 + 234*X + 2345*X^123 + X + 2;";
+    vector<std::string> expect1 = {"define", "Y", "=", "23", "*", "X", "^", "2", "+", "234", "*", "X", "+", "2345", "*", "X", "^", "123", "+", "X", "+", "2", ";"};
+    vector<TokenType> expect2 = {DEFINE, ID, EQ, NUMBER, MUL, ID, POWER, NUMBER, PLUS, NUMBER, MUL, ID, PLUS, NUMBER, MUL, ID, POWER, NUMBER, PLUS, ID, PLUS, NUMBER, SEMI};
+    dialog.command = command;
+    for(int i = 0; i < expect1.size(); i++){
+        dialog.getToken();
+        EXPECT_EQ(dialog.tokenString, expect1[i]);
+        EXPECT_EQ(dialog.token, expect2[i]);
+    }
+}
 
 int main() {
-    std::string command = "define Y = X;";
-    in
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
 }
